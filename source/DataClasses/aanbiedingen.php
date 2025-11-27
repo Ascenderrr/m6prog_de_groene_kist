82a4695fbfa294683a5f766bd69f5bf1dffa9d3a
Othman <o.boudaati@gmail.com>
2025-11-27 12:23:06 +0100

    boem

--- source/DataClasses/aanbiedingen.php
+++ source/DataClasses/aanbiedingen.php
@@ -4,10 +4,10 @@ require_once '/../database.php';
 class aanbiedingen 
 {
     public int $idaanbiedingen;
-    public ?int $korting;
+    public int $korting;
     public int $product_idproduct;
 
-    public function __construct(int $idaanbiedingen, ?int $korting, int $product_idproduct) 
+    public function __construct(int $idaanbiedingen, int $korting, int $product_idproduct) 
     {
         $this->idaanbiedingen = $idaanbiedingen;
         $this->korting = $korting;
